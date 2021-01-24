c26b31ed24b7f20df028354f27a52572e61ebf0c
kakatics <kakatics@gmail.com>
2021-01-24 11:51:35 +0100

    Drawing the Snake

--- game.py
+++ game.py
@@ -20,15 +20,44 @@ class Game(object):
     for y in range(0, Config.WINDOW_HEIGHT, Config.CELLSIZE):
       pygame.draw.line(self.screen, Config.DARKGRAY, (0, y), (Config.WINDOW_WIDTH, y))
 
+  def draw_worm(self):
+    for coord in self.snake.worm_coords:
+      x = coord['x'] * Config.CELLSIZE
+      y = coord['y'] * Config.CELLSIZE
+      worm_segment_rect = pygame.Rect(x, y, Config.CELLSIZE, Config.CELLSIZE)
+      pygame.draw.rect(self.screen, Config.DARKGREEN, worm_segment_rect)
+      worm_inner_segment_rect = pygame.Rect(x+4, y+4, Config.CELLSIZE-8, Config.CELLSIZE-8)
+      pygame.draw.rect(self.screen, Config.GREEN, worm_inner_segment_rect)
+
   def draw(self):
     self.screen.fill(Config.BG_COLOR)
     self.draw_grid()
+    self.draw_worm()
     pygame.display.update()
     self.clock.tick(Config.FPS)
 
   def handle_key_events(self, event):
     if event.key == pygame.K_ESCAPE:
       pygame.quit()
+
+  def is_game_over(self):
+    if (self.snake.worm_coords[self.snake.HEAD]['x'] == -1 or
+       self.snake.worm_coords[self.snake.HEAD]['x'] == Config.CELLWIDTH or
+       self.snake.worm_coords[self.snake.HEAD]['y'] == -1 or
+       self.snake.worm_coords[self.snake.HEAD]['y'] == Config.CELLHEIGHT):
+      return self.reset_game()
+    
+    for worm_body in self.snake.worm_coords[1:]:
+      if (worm_body['x'] == self.snake.worm_coords[self.snake.HEAD]['x'] and
+         worm_body['y'] == self.snake.worm_coords[self.snake.HEAD]['y']):
+        return self.reset_game()
+       
+  def reset_game(self):
+    del self.snake
+    del self.apple
+    self.snake = Snake()
+    self.apple = Apple()
+    return True
 
   def run(self):
     while True:
@@ -38,8 +67,8 @@ class Game(object):
         elif event.type == pygame.KEYDOWN:
           self.handle_key_events(event)
 
-      #self.snake.update(self.apple)
+      self.snake.update(self.apple)
       self.draw()
-      #if self.is_game_over():
-       # break
+      if self.is_game_over():
+        break
     
--- snake.py
+++ snake.py
@@ -17,5 +17,34 @@ class Snake(object):
         {'x': self.x, 'y': self.y},
         {'x': self.x-1, 'y': self.y},
         {'x': self.x-2, 'y': self.y},
-    ]
+        ]
+
+  def update(self, apple):
+    if self.worm_coords[self.HEAD]['x'] == apple.x and self.worm_coords[self.HEAD]['y'] == apple.y:
+      apple.set_new_location()
+    else:
+      del self.worm_coords[-1]
+
+    if self.direction == self.UP:
+      new_head = {
+          'x': self.worm_coords[self.HEAD]['x'],
+          'y': self.worm_coords[self.HEAD]['y'] - 1,
+          }
+    elif self.direction == self.DOWN:
+      new_head = {
+          'x': self.worm_coords[self.HEAD]['x'],
+          'y': self.worm_coords[self.HEAD]['y'] + 1,
+          }
+    elif self.direction == self.LEFT:
+      new_head = {
+          'x': self.worm_coords[self.HEAD]['x'] - 1,
+          'y': self.worm_coords[self.HEAD]['y'],
+          }
+    elif self.direction == self.RIGHT:
+      new_head = {
+          'x': self.worm_coords[self.HEAD]['x'] + 1,
+          'y': self.worm_coords[self.HEAD]['y'],
+          }
+
+    self.worm_coords.insert(0, new_head)
 
